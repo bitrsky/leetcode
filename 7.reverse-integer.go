f6416df1f6d0a46ca6252ed3911cc8f4591617b7
bitrsky <bitrsky@163.com>
2022-02-10 15:14:30 +0800

    reverse-integer

---
+++ 7.reverse-integer.go
@@ -0,0 +1,83 @@
+//package main
+
+import (
+	"math"
+)
+
+/*
+ * @lc app=leetcode id=7 lang=golang
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Medium (26.48%)
+ * Likes:    6544
+ * Dislikes: 9256
+ * Total Accepted:    1.9M
+ * Total Submissions: 7.3M
+ * Testcase Example:  '123'
+ *
+ * Given a signed 32-bit integer x, return x with its digits reversed. If
+ * reversing x causes the value to go outside the signed 32-bit integer range
+ * [-2^31, 2^31 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed
+ * or unsigned).
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: x = 123
+ * Output: 321
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: x = -123
+ * Output: -321
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: x = 120
+ * Output: 21
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= x <= 2^31 - 1
+ *
+ *
+ */
+
+/*
+* Node : -12 % 10 = -2
+ */
+// @lc code=start
+func reverse(x int) int {
+	ans := 0
+	for x != 0 {
+		ans = ans*10 + x%10
+		x /= 10
+	}
+	if ans > math.MaxInt32 || ans < math.MinInt32 {
+		return 0
+	}
+	return ans
+}
+
+/*
+func main() {
+	fmt.Println(-12 % 10)
+	fmt.Println(reverse(-12))
+}
+*/
+
+// @lc code=end
