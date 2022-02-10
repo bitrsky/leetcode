e9424fa15508a4eeae0fcaf00323a7dcac1a2106
bitrsky <bitrsky@163.com>
2022-02-10 17:02:20 +0800

    palindrome-number

---
+++ 9.palindrome-number.go
@@ -0,0 +1,83 @@
+//package main
+
+/*
+ * @lc app=leetcode id=9 lang=golang
+ *
+ * [9] Palindrome Number
+ *
+ * https://leetcode.com/problems/palindrome-number/description/
+ *
+ * algorithms
+ * Easy (52.10%)
+ * Likes:    5097
+ * Dislikes: 2019
+ * Total Accepted:    1.8M
+ * Total Submissions: 3.5M
+ * Testcase Example:  '121'
+ *
+ * Given an integer x, return true if x is palindrome integer.
+ *
+ * An integer is a palindrome when it reads the same backward as forward.
+ *
+ *
+ * For example, 121 is a palindrome while 123 is not.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it
+ * becomes 121-. Therefore it is not a palindrome.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a
+ * palindrome.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= x <= 2^31 - 1
+ *
+ *
+ *
+ * Follow up: Could you solve it without converting the integer to a string?
+ */
+
+// @lc code=start
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	reverse, ori := 0, x
+	for x != 0 {
+		reverse = reverse*10 + x%10
+		x /= 10
+	}
+	return reverse == ori
+}
+
+/*
+func main() {
+	fmt.Println(isPalindrome(121))
+}
+*/
+
+// @lc code=end
