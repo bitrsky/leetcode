165a4c535af214c702a428877812e4d299fa1f98
zhiwei <gzw@zpolar.com>
2022-03-19 18:57:18 +0800

    17,29

--- 17.letter-combinations-of-a-phone-number.go
+++ 17.letter-combinations-of-a-phone-number.go
@@ -1,3 +1,7 @@
+package main
+
+import "fmt"
+
 /*
  * @lc app=leetcode id=17 lang=golang
  *
@@ -16,46 +20,67 @@
  * Given a string containing digits from 2-9 inclusive, return all possible
  * letter combinations that the number could represent. Return the answer in
  * any order.
- * 
+ *
  * A mapping of digit to letters (just like on the telephone buttons) is given
  * below. Note that 1 does not map to any letters.
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: digits = "23"
  * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: digits = ""
  * Output: []
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: digits = "2"
  * Output: ["a","b","c"]
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 0 <= digits.length <= 4
  * digits[i] is a digit in the range ['2', '9'].
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
+var num2char = []string{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}
+
 func letterCombinations(digits string) []string {
-    
+	ans := []string{}
+	if len(digits) == 0 {
+		return []string{}
+	}
+	str := num2char[digits[0]-'0']
+	suffixs := letterCombinations(digits[1:])
+	if len(suffixs) == 0 {
+		suffixs = append(suffixs, "")
+	}
+	for _, s := range suffixs {
+		for i := 0; i < len(str); i++ {
+			ans = append(ans, string(append(append([]byte{}, str[i]), []byte(s)...)))
+		}
+	}
+	return ans
 }
+
 // @lc code=end
+func main() {
+
+	fmt.Println(letterCombinations("23"))
+}
