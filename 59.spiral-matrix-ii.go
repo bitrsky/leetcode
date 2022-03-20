2358a1ccd1bd2258ef8afb57f3b8fbb2333912b2
ubuntu <ubuntu@localhost.localdomain>
2022-03-20 12:02:32 +0800

    59

---
+++ 59.spiral-matrix-ii.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=59 lang=golang
+ *
+ * [59] Spiral Matrix II
+ *
+ * https://leetcode.com/problems/spiral-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (62.04%)
+ * Likes:    2635
+ * Dislikes: 163
+ * Total Accepted:    309.4K
+ * Total Submissions: 498.6K
+ * Testcase Example:  '3'
+ *
+ * Given a positive integer n, generate an n x n matrix filled with elements
+ * from 1 to n^2 in spiral order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 3
+ * Output: [[1,2,3],[8,9,4],[7,6,5]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 1
+ * Output: [[1]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 20
+ * 
+ * 
+ */
+
+// @lc code=start
+func generateMatrix(n int) [][]int {
+    
+}
+// @lc code=end
