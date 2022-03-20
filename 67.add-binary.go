10800906e16c9d9da8ed3d99d0ab054f629896a6
ubuntu <ubuntu@localhost.localdomain>
2022-03-20 13:32:53 +0800

    62-70

---
+++ 67.add-binary.go
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=67 lang=golang
+ *
+ * [67] Add Binary
+ *
+ * https://leetcode.com/problems/add-binary/description/
+ *
+ * algorithms
+ * Easy (50.21%)
+ * Likes:    4721
+ * Dislikes: 522
+ * Total Accepted:    802.8K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '"11"\n"1"'
+ *
+ * Given two binary strings a and b, return their sum as a binary string.
+ * 
+ * 
+ * Example 1:
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * Example 2:
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= a.length, b.length <= 10^4
+ * a and b consist only of '0' or '1' characters.
+ * Each string does not contain leading zeros except for the zero itself.
+ * 
+ * 
+ */
+
+// @lc code=start
+func addBinary(a string, b string) string {
+    
+}
+// @lc code=end
